836ad8ea25a2cd2ce2f6af4ad2c08d8945ef0b9f
Manish Prajapati <marveluniverse1942@gmail.com>
2025-02-14 12:06:16 +0530

    initial commit

---
+++ Frontend/src/Components/Dashboard/Login&Register.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Login from '../Auth/Login'
+
+const Login_Register = () => {
+  return (
+    <>
+      <div>
+        <Login />
+      </div>
+
+    </>
+  )
+}
+
+export default Login_Register
