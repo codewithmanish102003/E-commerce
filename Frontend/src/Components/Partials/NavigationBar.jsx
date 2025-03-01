4e84bb88e5cd9b3dcab116bf62d563570bee9c85
Manish Prajapati <marveluniverse1942@gmail.com>
2025-03-01 21:54:38 +0530

    bug fix

--- Frontend/src/Components/Partials/NavigationBar.jsx
+++ Frontend/src/Components/Partials/NavigationBar.jsx
@@ -149,7 +149,7 @@ const NavigationBar = () => {
                         {isLoggedIn ? (
                             <button 
                                 onClick={handleLogout} 
-                                className="w-full text-left text-gray-900 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-base font-medium "
+                                className="w-full text-left text-gray-900 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-base font-mediummflex flex items-center gap-2"
                             >
                                 <LogOut className="h-4 w-4" />
                                 Logout
